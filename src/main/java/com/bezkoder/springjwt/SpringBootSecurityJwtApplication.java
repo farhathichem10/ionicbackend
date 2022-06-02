020f2d87c7a100978c7f6a881ab7e16d928a7aa8
Hichem Farhat <Hichem.Farhat@teriak.local>
2022-06-02 11:29:36 +0200

    final hhh

--- src/main/java/com/bezkoder/springjwt/SpringBootSecurityJwtApplication.java
+++ src/main/java/com/bezkoder/springjwt/SpringBootSecurityJwtApplication.java
@@ -1,12 +1,21 @@
 package com.bezkoder.springjwt;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bezkoder.springjwt.models.ERole;
+import com.bezkoder.springjwt.models.Role;
+import com.bezkoder.springjwt.repository.RoleRepository;
+
 @SpringBootApplication
 public class SpringBootSecurityJwtApplication {
+	
 
 	public static void main(String[] args) {
+		
+		
+		
     SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
 	}
 
